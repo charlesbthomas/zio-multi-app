2973bc469d57c6d8e0ef4e30b615a3121aad573f
charlie <contact@charliebthomas.com>
2023-02-04 19:11:11 -0600

    fmt

--- libs/utils/src/main/scala/dev/astern/http/AsternHttpServer.scala
+++ libs/utils/src/main/scala/dev/astern/http/AsternHttpServer.scala
@@ -4,16 +4,17 @@ import zhttp.http.*
 import zhttp.service.Server
 import zio.*
 
- trait AsternHttpServer:
+trait AsternHttpServer:
 
   def apps: Seq[AsternHttpApp]
 
-  private val allRoutes = apps.map(_.routes).reduce((a,b) => a ++ b)
+  private val allRoutes = apps.map(_.routes).reduce((a, b) => a ++ b)
 
   def start: ZIO[RequestContextManager, Throwable, Unit] =
-    for 
-      _      <- ZIO.succeed(println("Starting HTTP Server"))
-      port   <- System.envOrElse("PORT", "8080").map(_.toInt)
-      _      <- Server.start(port, allRoutes) <* ZIO.succeed(println(s"HTTP Server listening on port $port"))
+    for
+      _ <- ZIO.succeed(println("Starting HTTP Server"))
+      port <- System.envOrElse("PORT", "8080").map(_.toInt)
+      _ <- Server.start(port, allRoutes) <* ZIO.succeed(
+        println(s"HTTP Server listening on port $port")
+      )
     yield ()
-
--- libs/utils/src/main/scala/dev/astern/http/RequestContextManager.scala
+++ libs/utils/src/main/scala/dev/astern/http/RequestContextManager.scala
@@ -1,20 +1,22 @@
 package dev.astern.http
 
+import zio.FiberRef
+import zio.UIO
+import zio.ULayer
+import zio.ZLayer
 
-import zio.{FiberRef, UIO, ULayer, ZLayer}
 trait RequestContextManager {
   def setCtx(ctx: RequestContext): UIO[Unit]
   def getCtx(): UIO[RequestContext]
 }
 
 final case class DefaultRequestContextManager(ref: FiberRef[RequestContext])
-  extends RequestContextManager:
-    override def getCtx(): UIO[RequestContext] = ref.get
-    override def setCtx(ctx: RequestContext): UIO[Unit] = ref set ctx
+    extends RequestContextManager:
+  override def getCtx(): UIO[RequestContext] = ref.get
+  override def setCtx(ctx: RequestContext): UIO[Unit] = ref set ctx
 
 object DefaultRequestContextManager:
   def layer: ULayer[RequestContextManager] = ZLayer.scoped {
-    for 
-      ref <- FiberRef.make(RequestContext.initial)
+    for ref <- FiberRef.make(RequestContext.initial)
     yield DefaultRequestContextManager(ref)
   }
